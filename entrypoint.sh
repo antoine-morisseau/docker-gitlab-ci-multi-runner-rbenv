892f5a5c6ad5ff270b108ad6fb3d906b1ee75ece
Antoine Morisseau <Morisseau.Antoine@gmail.com>
2017-12-02 00:57:05 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+set -e
+
+CA_CERTIFICATES_PATH=${CA_CERTIFICATES_PATH:-$GITLAB_RUNNER_DATA_DIR/certs/ca.crt}
+
+create_data_dir() {
+  mkdir -p ${GITLAB_RUNNER_DATA_DIR}
+  chown ${GITLAB_RUNNER_USER}:${GITLAB_RUNNER_USER} ${GITLAB_RUNNER_DATA_DIR}
+}
+
+generate_ssh_deploy_keys() {
+  sudo -HEu ${GITLAB_RUNNER_USER} mkdir -p ${GITLAB_RUNNER_DATA_DIR}/.ssh/
+
+  if [[ ! -e ${GITLAB_RUNNER_DATA_DIR}/.ssh/id_rsa || ! -e ${GITLAB_RUNNER_DATA_DIR}/.ssh/id_rsa.pub ]]; then
+    echo "Generating SSH deploy keys..."
+    rm -rf ${GITLAB_RUNNER_DATA_DIR}/.ssh/id_rsa ${GITLAB_RUNNER_DATA_DIR}/.ssh/id_rsa.pub
+    sudo -HEu ${GITLAB_RUNNER_USER} ssh-keygen -t rsa -N "" -f ${GITLAB_RUNNER_DATA_DIR}/.ssh/id_rsa
+
+    echo ""
+    echo -n "Your SSH deploy key is: "
+    cat ${GITLAB_RUNNER_DATA_DIR}/.ssh/id_rsa.pub
+    echo ""
+  fi
+
+  chmod 600 ${GITLAB_RUNNER_DATA_DIR}/.ssh/id_rsa ${GITLAB_RUNNER_DATA_DIR}/.ssh/id_rsa.pub
+  chmod 700 ${GITLAB_RUNNER_DATA_DIR}/.ssh
+  chown -R ${GITLAB_RUNNER_USER}:${GITLAB_RUNNER_USER} ${GITLAB_RUNNER_DATA_DIR}/.ssh/
+}
+
+update_ca_certificates() {
+  if [[ -f ${CA_CERTIFICATES_PATH} ]]; then
+    echo "Updating CA certificates..."
+    cp "${CA_CERTIFICATES_PATH}" /usr/local/share/ca-certificates/ca.crt
+    update-ca-certificates --fresh >/dev/null
+  fi
+}
+
+grant_access_to_docker_socket() {
+  if [ -S /run/docker.sock ]; then
+    DOCKER_SOCKET_GID=$(stat -c %g  /run/docker.sock)
+    DOCKER_SOCKET_GROUP=$(stat -c %G /run/docker.sock)
+    if [[ ${DOCKER_SOCKET_GROUP} == "UNKNOWN" ]]; then
+      DOCKER_SOCKET_GROUP=docker
+      groupadd -g ${DOCKER_SOCKET_GID} ${DOCKER_SOCKET_GROUP}
+    fi
+    usermod -a -G ${DOCKER_SOCKET_GROUP} ${GITLAB_RUNNER_USER}
+  fi
+}
+
+configure_config_tom() {
+  if [[ ! -e ${GITLAB_RUNNER_DATA_DIR}/config.toml ]]; then
+      sudo -HEu ${GITLAB_RUNNER_USER} \
+        gitlab-ci-multi-runner register --config ${GITLAB_RUNNER_DATA_DIR}/config.toml
+  fi
+}
+
+configure_ci_runner() {
+  if [[ ! -e ${GITLAB_RUNNER_DATA_DIR}/config.toml ]]; then
+      sudo -HEu ${GITLAB_RUNNER_USER} \
+        gitlab-runner register --config ${GITLAB_RUNNER_DATA_DIR}/config.toml
+  fi
+}
+
+# allow arguments to be passed to gitlab-ci-multi-runner
+if [[ ${1:0:1} = '-' ]]; then
+  EXTRA_ARGS="$@"
+  set --
+elif [[ ${1} == gitlab-runner || ${1} == $(which gitlab-runner) ]]; then
+  EXTRA_ARGS="${@:2}"
+  set --
+fi
+
+# default behaviour is to launch gitlab-ci-multi-runner
+if [[ -z ${1} ]]; then
+  create_data_dir
+  update_ca_certificates
+  generate_ssh_deploy_keys
+  grant_access_to_docker_socket
+  configure_config_toml
+  configure_ci_runner
+
+  start-stop-daemon --start \
+    --chuid ${GITLAB_RUNNER_USER}:${GITLAB_RUNNER_USER} \
+    --exec $(which gitlab-runner) -- run \
+      --working-directory ${GITLAB_RUNNER_DATA_DIR} \
+      --config ${GITLAB_RUNNER_DATA_DIR}/config.toml ${EXTRA_ARGS}
+else
+  exec "$@"
+fi
